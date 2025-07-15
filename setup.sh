f6b06081b1754e265a14aac0f962a4d898a08280
rasim ghodrati <122753773+rasim-gh@users.noreply.github.com>
2025-07-15 17:05:08 +0330

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
-
 set -e
 
 echo "🚀 Downloading Cloudflare DNS Bot..."
 
-# مسیر نصب (پوشه مقصد)
 INSTALL_DIR="/root/cloudflare_dns_bot"
-
-# اگه از قبل وجود داره، پاکش کن
 rm -rf $INSTALL_DIR
-
-# کلون ریپو
 git clone https://github.com/rasim-gh/cloudflare_dns_bot.git $INSTALL_DIR
-
-# رفتن داخل پوشه
 cd $INSTALL_DIR
-
-# اجرای اسکریپت نصب
 bash install.sh
